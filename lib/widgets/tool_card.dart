922d67590b3dfb282fbf7b8b71cc885033f47b20
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-19 21:23:00 +0300

    code organization

--- lib/widgets/tool_card.dart
+++ lib/widgets/tool_card.dart
@@ -7,90 +7,75 @@ import 'package:project/screens/APK_Analyzer_screen.dart';
 
 class ToolCard extends StatelessWidget {
   final Tool tool;
-  const ToolCard({
-    super.key,
-    required this.tool,
-  });
+  const ToolCard({super.key, required this.tool});
   @override
   Widget build(BuildContext context) {
     return Card(
       color: Colors.grey[900],
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-  elevation: 5,
-  child: InkWell(
-  onTap: () {
-  if (tool.title == 'Password Generator') {
-  Navigator.push(
-  context,
-  MaterialPageRoute(
-  builder: (context) => const PasswordGeneratorScreen(),
-  ),
-  );
-  }
-  else if (tool.title == 'Port Scanner') {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const PortScannerScreen(),
-      ),
-    );
-  }
-
-  else if (tool.title == 'APK Analyzer') {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const APKAnalyzerScreen(),
-      ),
-    );
-  }
-  else if (tool.title == 'HTTP Header Analyzer') {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const HTTPHeaderAnalyzerScreen(),
-      ),
-    );
-  }
-  },
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              tool.icon,
-              size: 50,
-              color: Colors.greenAccent[400],
-            ),
-            const SizedBox(height: 10),
-            Text(
-              tool.title,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      elevation: 5,
+      child: InkWell(
+        onTap: () {
+          if (tool.title == 'Password Generator') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const PasswordGeneratorScreen(),
               ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 5),
-            Text(
-              tool.description,
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.white.withOpacity(0.7),
+            );
+          } else if (tool.title == 'Port Scanner') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const PortScannerScreen(),
               ),
-              textAlign: TextAlign.center,
-            ),
-          ],
+            );
+          } else if (tool.title == 'APK Analyzer') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const APKAnalyzerScreen(),
+              ),
+            );
+          } else if (tool.title == 'HTTP Header Analyzer') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HTTPHeaderAnalyzerScreen(),
+              ),
+            );
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(tool.icon, size: 50, color: Colors.greenAccent[400]),
+              const SizedBox(height: 10),
+              Text(
+                tool.title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                tool.description,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white.withOpacity(0.7),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
-  ),
     );
   }
 }
-
-
