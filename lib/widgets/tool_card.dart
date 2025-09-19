30c8749712b8729678084e391f5fc030e5e42df5
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-19 21:02:20 +0300

    add ToolCard class in tool_card.dart

--- lib/widgets/tool_card.dart
+++ lib/widgets/tool_card.dart
@@ -4,3 +4,12 @@ import 'package:project/screens/pass_generator_screen.dart';
 import 'package:project/screens/port_scanner_screen.dart';
 import 'package:project/screens/HTTP_Header_Analyzer_Screen.dart';
 import 'package:project/screens/APK_Analyzer_screen.dart';
+
+class ToolCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.grey[900],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),);}}
