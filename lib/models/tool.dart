37dea5633358146fff468fd193dfdfe1b4283f33
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-19 20:55:43 +0300

    add class tool in tool.dart

--- lib/models/tool.dart
+++ lib/models/tool.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+class Tool {
+  final String title;
+  final String description;
+  final IconData icon;
+
+  const Tool({
+    required this.title,
+    required this.description,
+    required this.icon,
+  });
+}
