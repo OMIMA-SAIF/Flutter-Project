2c5b33e9a16ddd7244e2924063796f92b3d5e784
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-19 20:50:22 +0300

    add class MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
