62283146d0be07afa42f73f6de5a0d1927cdb9e9
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-20 18:28:16 +0300

    adding AppBar in home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,6 +27,29 @@ class HomeScreen extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'stra Tools',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+          ),
+        ),
+        centerTitle: true,
+
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings, color: Colors.greenAccent[400]),
+            onPressed: () {
+              // ...
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
