f8d91c3f629f2ce0f0572073bd9efbc5ee0582d9
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-20 18:26:47 +0300

    feat (home_screen) add a list of toole to main screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,5 +3,26 @@ import 'package:project/models/tool.dart';
 import 'package:project/widgets/tool_card.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
+  final List<Tool> tools = const [
+    Tool(
+      title: 'Password Generator',
+      description: 'Create strong and random passwords.',
+      icon: Icons.lock_outline,
+    ),
+    Tool(
+      title: 'Port Scanner',
+      description: 'Scan open ports on a specific IP.',
+      icon: Icons.network_check_outlined,
+    ),
+    Tool(
+      title: 'APK Analyzer',
+      description: 'View app permissions and details.',
+      icon: Icons.android_outlined,
+    ),
+    Tool(
+      title: 'HTTP Header Analyzer',
+      description: 'Analyze a website\'s headers.',
+      icon: Icons.code,
+    ),
+  ];
 }
