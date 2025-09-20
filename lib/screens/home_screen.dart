fd0c230d7de75af2e18c631eb653cfd989df41cb
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-20 18:29:25 +0300

    organization the code in home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project/models/tool.dart';
 import 'package:project/widgets/tool_card.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   final List<Tool> tools = const [
