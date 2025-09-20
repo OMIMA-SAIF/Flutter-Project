0f8ce9d9140f85ae6dc5565f4e5c97c0dd30ea21
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-20 17:28:46 +0300

    feat pass_generator_screen.dart to generate passwords

--- lib/screens/pass_generator_screen.dart
+++ lib/screens/pass_generator_screen.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class PasswordGeneratorScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+
+}
