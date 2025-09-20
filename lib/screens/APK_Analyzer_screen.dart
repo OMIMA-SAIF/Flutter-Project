b0ceddd43fbacf8540dd64d36b78f555ab792b8d
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-20 17:48:17 +0300

    create a new scanner scaffold

--- lib/screens/APK_Analyzer_screen.dart
+++ lib/screens/APK_Analyzer_screen.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+class APKAnalyzerScreen extends StatelessWidget {
+  const APKAnalyzerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+
+  }
+}
