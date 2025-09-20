cce6377935260b375268d97df88c810c3bc01431
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-20 17:38:32 +0300

    create a new scanner scaffold

--- lib/screens/port_scanner_screen.dart
+++ lib/screens/port_scanner_screen.dart
@@ -1,1 +1,9 @@
 import 'package:flutter/material.dart';
+class PasswordGeneratorScreen extends StatelessWidget {
+  const PasswordGeneratorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
