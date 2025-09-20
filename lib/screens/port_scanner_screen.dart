85ef1bd860ffd10121ce177ecd557660ba8641b1
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-20 17:42:15 +0300

    organization the code in port_scanner_screen.dart

--- lib/screens/port_scanner_screen.dart
+++ lib/screens/port_scanner_screen.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+
 class PasswordGeneratorScreen extends StatelessWidget {
   const PasswordGeneratorScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      title: const Text('Password Generator'),
-      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        title: const Text('Password Generator'),
+        backgroundColor: Colors.transparent,
+      ),
       body: const Center(
         child: Text(
           'This is the Password Generator page.',
           style: TextStyle(color: Colors.white, fontSize: 18),
         ),
       ),
-    ),
     );
   }
 }
