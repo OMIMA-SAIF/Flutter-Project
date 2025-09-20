2e650154b651fed49d7d0171fe9346ae3867df9a
OMIMA-SAIF <omima0saif@gmail.com>
2025-09-20 17:54:29 +0300

    create a new scanner scaffold

--- lib/screens/HTTP_Header_Analyzer_Screen.dart
+++ lib/screens/HTTP_Header_Analyzer_Screen.dart
@@ -1,1 +1,9 @@
 import 'package:flutter/material.dart';
+class HTTPHeaderAnalyzerScreen extends StatelessWidget {
+  const HTTPHeaderAnalyzerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
