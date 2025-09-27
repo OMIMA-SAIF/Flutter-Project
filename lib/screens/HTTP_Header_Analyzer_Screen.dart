0011f45e32786734ff40bfad4afe8cbaf3641073
amal_obaid <amalobaid230gmail.com>
2025-09-27 16:31:28 +0300

    import convert to HTTP_Header file

--- lib/screens/HTTP_Header_Analyzer_Screen.dart
+++ lib/screens/HTTP_Header_Analyzer_Screen.dart
@@ -1,6 +1,7 @@
 // lib/screens/password_generator_screen.dart
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HTTPHeaderAnalyzerScreen extends StatelessWidget {
   const HTTPHeaderAnalyzerScreen({super.key});
