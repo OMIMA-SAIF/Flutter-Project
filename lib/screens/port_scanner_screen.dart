4aaee2b322339626c8ca83dc9f839071ca0692f4
amal_obaid <amalobaid230gmail.com>
2025-09-27 16:50:00 +0300

    add the first override

--- lib/screens/port_scanner_screen.dart
+++ lib/screens/port_scanner_screen.dart
@@ -6,18 +6,5 @@ class PortScannerScreen extends StatelessWidget {
   const PortScannerScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Port Scanner'),
-        backgroundColor: Colors.transparent,
-      ),
-      body: const Center(
-        child: Text(
-          'This is the Port Scanner page.',
-          style: TextStyle(color: Colors.white, fontSize: 18),
-        ),
-      ),
-    );
-  }
+  State<PortScannerScreen> createState() => _PortScannerScreenState();
 }
